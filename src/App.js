a845669941156e401777f31221708b2333413980
Girish@0201 <bathigirish@gmail.com>
2023-05-15 16:43:50 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,39 @@
+/*
+const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+]
+  */
+
+import {Route, Switch} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+import RestaurantDetails from './components/RestaurantDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <ProtectedRoute
+      exact
+      path="/restaurant/:id"
+      component={RestaurantDetails}
+    />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
